86140d2470b5e69d4c846ba08aded33800c385eb
Bryce Eppler <eppler97@gmail.com>
2022-10-22 09:29:07 -0700

    fixing hover color on catalog row

--- src/components/SearchRow.jsx
+++ src/components/SearchRow.jsx
@@ -22,7 +22,7 @@ export default function SearchRow({ cardData }) {
   return (
     <>
         <>
-          <div className="grid grid-cols-12 gap-4 m-1 p-2 hover:bg-darkerBackground rounded-md">
+          <div className="grid grid-cols-12 gap-4 m-1 p-2 hover:backdrop-brightness-75 rounded-md">
             <div className="col-span-3">
               <img
                 src={cardData.image}
