9c4509c6a8252155cc87aae57886b05b6eaf4d21
Bryce Eppler <eppler97@gmail.com>
2022-10-24 12:10:30 -0700

    styling changes

--- src/components/VariantSelectorModal.jsx
+++ src/components/VariantSelectorModal.jsx
@@ -31,7 +31,7 @@ export default function VariantSelectorModal({
         } fixed inset-0 z-20 flex items-center justify-center`}
       >
         {/* Modal Content */}
-        <div className="bg-white dark:bg-darkBackground w-11/12 md:w-1/2 lg:w-1/3 rounded-lg shadow-lg z-30">
+        <div className="bg-white dark:bg-darkBackground w-11/12 md:w-2/3 lg:w-1/3 rounded-lg shadow-lg z-30">
           {/* Modal Header */}
           <div className="flex justify-between items-center p-4 border-b">
             <h3 className="text-2xl font-bold">Select a variant</h3>
@@ -52,19 +52,25 @@ export default function VariantSelectorModal({
                     <img
                       src={variant.image}
                       alt="card"
-                      className="w-16 h-24 rounded-sm"
+                      className="w-16 sm:w-24 rounded-sm sm:rounded-md"
                     />
                     <div className="ml-2">
-                      <div>{variant.name}</div>
-                      <div>{variant.set}</div>
-                      <div>{variant.condition}</div>
-                      {variant.foil && <div>Foil</div>}
-                      <div>{variant.price}</div>
-                      <div>{variant.website}</div>
+                      <div className="text-md font-bold">{variant.name}</div>
+                      <div className="text-sm">{variant.set}</div>
+                     
+                      <div className="text-sm">{variant.website}</div>
                     </div>
                   </div>
+                  <div className="flex flex-col items-end">
+                    <div className="flex flex-row space-x-2">
+                    {variant.foil && <div className="text-sm font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-primary to-purple-500">Foil</div>}
+
+                  </div>
+                      <div className="text-md font-bold">${variant.price}</div>
+                      <div className="text-sm font-bold">{variant.condition}</div>
+
                   <button
-                    className="bg-primary hover:bg-purple-500 text-white font-bold py-2 px-4 rounded"
+                    className="bg-primary hover:bg-purple-500 text-white font-bold p-1 rounded-md w-20"
                     onClick={() => {
                         // update the selectedBulkInfo to reflect the price change
                         if (isSelected) {
@@ -84,6 +90,7 @@ export default function VariantSelectorModal({
                   >
                     Select
                   </button>
+                  </div>
                 </div>
               ))}
             </div>
