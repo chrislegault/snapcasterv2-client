32f147e2ac3410ade9141e3fa6b077f73d0ae62a
Bryce Eppler <eppler97@gmail.com>
2022-10-27 05:03:38 -0700

    homepage header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="text-center">
+      <div className="text-xl font-bold m-5">
+        Search for Magic: the Gathering singles in Canada
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import SingleCardSearch from '../components/SingleCardSearch';
 import { useAtomValue } from 'jotai';
 import { hideWelcomeMessage } from '../atoms';
+import Header from '../components/Header';
 
 export default function Home() {
   const hideWelcomeMessageValue = useAtomValue(hideWelcomeMessage);
   return (
     <>
       <div className="max-w-xl mx-auto">
+        {!hideWelcomeMessageValue && <Header />}
         <SingleCardSearch />
       </div>
     </>
