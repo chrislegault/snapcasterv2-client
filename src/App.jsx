06b9c6347dd05f3695253ce21b4bfe021db887bd
Bryce Eppler <eppler97@gmail.com>
2022-10-28 10:46:40 -0700

    adding template for sealed search

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import MultiSearch from './pages/MultiSearch';
 import Layout from './Layout';
 import About from './pages/About';
+import SealedSearch from './pages/SealedSearch';
 import { Provider as JotaiProvider } from 'jotai';
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/multi-search" element={<MultiSearch />} />
               <Route path="/about" element={<About />} />
+              <Route path="/sealed-search" element={<SealedSearch />} />
             </Routes>
           </Layout>
       </JotaiProvider>
