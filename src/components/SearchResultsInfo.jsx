3ed8ca1cdaa5483229ca113fc01cc389f615bc59
Bryce Eppler <eppler97@gmail.com>
2022-10-29 12:10:44 -0700

    making results info box look better

--- src/components/SearchResultsInfo.jsx
+++ src/components/SearchResultsInfo.jsx
@@ -5,7 +5,7 @@ import {
   sortOrderAtom,
   listViewAtom,
   filteredSingleCardResults,
-  foilFilterAtom
+  foilFilterAtom,
 } from '../atoms';
 import { sortResults } from '../utils';
 
@@ -21,13 +21,21 @@ export default function SearchResultsInfo({ numResults, searchTerm }) {
 
   const handleSortByChange = e => {
     setSortedBy(e.target.value);
-    const sortedResults = sortResults(filteredResults, e.target.value, sortOrder);
+    const sortedResults = sortResults(
+      filteredResults,
+      e.target.value,
+      sortOrder,
+    );
     setFilteredResults(sortedResults);
   };
 
   const handleSortOrderChange = e => {
     setSortOrder(e.target.value);
-    const sortedResults = sortResults(filteredResults, sortedBy, e.target.value);
+    const sortedResults = sortResults(
+      filteredResults,
+      sortedBy,
+      e.target.value,
+    );
     setFilteredResults(sortedResults);
   };
 
@@ -55,12 +63,12 @@ export default function SearchResultsInfo({ numResults, searchTerm }) {
       {/* Background */}
       <div className="bg-gray-100 dark:bg-darkerBackground rounded-md p-1 md:p-3">
         {/* Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-          <div className="mx-2 md:col-span-3">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 text-sm md:text-md">
+          <div className="mx-2 col-span-3">
             {numResults} results found for "{searchTerm}"
           </div>
           {/* Sort selector */}
-          <div className="flex flex-row mx-2 md:col-span-2">
+          <div className="flex flex-row mx-2 col-span-3">
             <div className="mr-2">Sort by:</div>
             <select
               className="bg-white dark:bg-gray-700 rounded-md border mx-1"
@@ -80,46 +88,48 @@ export default function SearchResultsInfo({ numResults, searchTerm }) {
               <option value="desc">Descending</option>
             </select>
           </div>
-          <div>
-            {/* foil toggle */}
-            <div className="flex items-center md:col-span-1">
-              <span className="mx-2">Foil:</span>
-              <label
-                htmlFor="foil-toggle"
-                className="inline-flex relative items-center cursor-pointer"
-              >
-                <input
-                  type="checkbox"
-                  value=""
-                  checked={foilFilter}
-                  id="foil-toggle"
-                  className="sr-only peer"
-                  onClick={toggleFoilFilter}
-                />
-                <div className="w-7 h-4 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-primary dark:peer-focus:ring-primary rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
-              </label>
+          {/* Toggles */}
+          <div className="flex flex-row col-span-3">
+            <div className="col-span-1 flex flex-row">
+              {/* foil toggle */}
+              <div className="flex items-center">
+                <span className="mx-2">Foil:</span>
+                <label
+                  htmlFor="foil-toggle"
+                  className="inline-flex relative items-center cursor-pointer"
+                >
+                  <input
+                    type="checkbox"
+                    value=""
+                    checked={foilFilter}
+                    id="foil-toggle"
+                    className="sr-only peer"
+                    onClick={toggleFoilFilter}
+                  />
+                  <div className="w-7 h-4 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-primary dark:peer-focus:ring-primary rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
+                </label>
+              </div>
+              {/* list view toggle */}
+              <div className="flex items-center">
+                <span className="mx-2">List View:</span>
+                <label
+                  htmlFor="list-view-toggle"
+                  className="inline-flex relative items-center cursor-pointer"
+                >
+                  <input
+                    type="checkbox"
+                    value=""
+                    checked={listView}
+                    id="list-view-toggle"
+                    className="sr-only peer"
+                    onClick={toggleListView}
+                  />
+                  <div className="w-7 h-4 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-primary dark:peer-focus:ring-primary rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
+                </label>
+              </div>
             </div>
           </div>
-          <div>
-            {/* list view toggle */}
-            <div className="flex items-center md:col-span-1">
-              <span className="mx-2">List View:</span>
-              <label
-                htmlFor="list-view-toggle"
-                className="inline-flex relative items-center cursor-pointer"
-              >
-                <input
-                  type="checkbox"
-                  value=""
-                  checked={listView}
-                  id="list-view-toggle"
-                  className="sr-only peer"
-                  onClick={toggleListView}
-                />
-                <div className="w-7 h-4 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-primary dark:peer-focus:ring-primary rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
-              </label>
-            </div>
-          </div>
+          <div></div>
         </div>
       </div>
     </>
