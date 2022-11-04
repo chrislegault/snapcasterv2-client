896a41cc11ec6c651d344cad3019a78899a18cb3
Bryce Eppler <eppler97@gmail.com>
2022-11-04 08:36:44 -0700

    adding tags

--- src/components/SealedCatalogRow.jsx
+++ src/components/SealedCatalogRow.jsx
@@ -44,7 +44,7 @@ export default function SealedCatalogRow({ product }) {
   return (
     <>
       <div className="grid grid-cols-12 gap-4 m-1 p-2 hover:backdrop-brightness-75 rounded-md">
-        <div className="col-span-3 mx-auto">
+        <div className="col-span-3 m-auto">
           <img
             src={product.image}
             alt={product.name}
@@ -57,11 +57,23 @@ export default function SealedCatalogRow({ product }) {
             <div className="text-sm dark:text-gray-400 ">
               {product.language}
             </div>
-            <img
-              src={websiteLogos[product.website]}
-              alt={product.website}
-              className="mt-1 w-12 md:w-20 object-cover"
-            />
+            <div className="flex flex-row">
+            {/* for each tag in product.tag, create a little tag card with a gradient background */}
+            {
+              product.tags.map((tag) => (
+                <div
+                  key={tag}
+                  className="text-xs font-bold rounded-md px-2 py-1 mt-1 mr-1"
+                  style={{
+                    background: `linear-gradient(90deg, red 0%, orange 100%)`,
+                  }}
+                >
+                  {tag}
+                </div>
+              ))
+            }
+            </div>
+
           </div>
         </div>
         <div className="col-span-4 mt-2">
@@ -69,8 +81,13 @@ export default function SealedCatalogRow({ product }) {
             <div className="text-lg font-bold">{product.price}</div>
             {/* if product.stock >= 1, show the stock, otherwise show a green checkmark */}
             <div className="text-xs font-bold">
-              In stock: {product.stock >= 1 ? product.stock : '✅'}
+              In stock: {product.stock >= 1 ? product.stock : '✓'}
             </div>
+            <img
+              src={websiteLogos[product.website]}
+              alt={product.website}
+              className="mt-1 w-12 md:w-12 object-cover"
+            />
             <button
               className="btn-small mt-2"
               // onClick - open product.link in a new tab
